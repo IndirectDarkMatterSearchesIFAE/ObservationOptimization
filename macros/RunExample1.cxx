c446a7e4db5d636f3002046663478dd9841d2f26
David <david.navarro.girones@gmail.com>
2017-07-03 17:38:44 +0200

    Some includes

--- macros/RunExample1.cxx
+++ macros/RunExample1.cxx
@@ -1,4 +1,10 @@
 #include <../source/Optimitzation.cc>
+#include </home/david/Work/Software/scripts/style.h>
+
+#include <TLegend.h>
+#include <TCanvas.h>
+#include <TF1.h>
+
 using namespace std;
 
 
